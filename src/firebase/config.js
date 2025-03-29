45650cddb59ea3c48d93e3d6c1f6aec7c53fd882
Luis Benitez <luisbenitez544@gmail.com>
2025-03-28 22:01:40 -0500

    config env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,20 +2,48 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
+import { getEnvironments } from "../helpers";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnvironments();
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDT0L7U67kMv7n4w1j9B7KcXEnOtYkHnTk",
+//   authDomain: "journal-app-a8b15.firebaseapp.com",
+//   projectId: "journal-app-a8b15",
+//   storageBucket: "journal-app-a8b15.firebasestorage.app",
+//   messagingSenderId: "444044636697",
+//   appId: "1:444044636697:web:84da9ebf9ab639de5eb088",
+//   measurementId: "G-943RRLEXRC",
+// };
+//Testing
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAR_3e21yhwigEsTA3xxSDNg99yYahJliE",
+//   authDomain: "bd-pruebas-dc373.firebaseapp.com",
+//   projectId: "bd-pruebas-dc373",
+//   storageBucket: "bd-pruebas-dc373.firebasestorage.app",
+//   messagingSenderId: "400656756656",
+//   appId: "1:400656756656:web:96c2b5df33e79751587075",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDT0L7U67kMv7n4w1j9B7KcXEnOtYkHnTk",
-  authDomain: "journal-app-a8b15.firebaseapp.com",
-  projectId: "journal-app-a8b15",
-  storageBucket: "journal-app-a8b15.firebasestorage.app",
-  messagingSenderId: "444044636697",
-  appId: "1:444044636697:web:84da9ebf9ab639de5eb088",
-  measurementId: "G-943RRLEXRC",
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
+console.log(firebaseConfig);
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
--- src/test/store/journal/thunks.test.js
+++ src/test/store/journal/thunks.test.js
@@ -1,4 +1,11 @@
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+} from "../../../store/journal";
 import { startNewNote } from "../../../store/journal/thunks";
+import { FirebaseDB } from "../../../firebase/config";
 
 describe("Pruebas en Journal Thunks", () => {
   const dispatch = jest.fn();
@@ -11,7 +18,40 @@ describe("Pruebas en Journal Thunks", () => {
       auth: {
         uid: uid,
       },
+
+      // el uid es el id del usuario que esta autenticado, el
+      //  mockReturnValue devuelve el estado de la autenticacion
     });
-    await startNewNote()(dispatch, getState);
+    await startNewNote()(dispatch, getState); // se envia el dispatch y el getState para que la función pueda acceder al estado
+    expect(dispatch).toHaveBeenCalledWith(savingNewNote());
+    expect(dispatch).toHaveBeenCalledWith(
+      addNewEmptyNote({
+        body: "",
+        title: "",
+        id: expect.any(String),
+        date: expect.any(Number),
+      }) // se espera que se llame a la acción addNewEmptyNote con el objeto de la nueva nota
+    );
+    expect(dispatch).toHaveBeenCalledWith(
+      setActiveNote({
+        body: "",
+        title: "",
+        id: expect.any(String),
+        date: expect.any(Number),
+      })
+    );
+    const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`);
+    const docs = await getDocs(collectionRef);
+
+    const deletePromises = [];
+    docs.forEach((doc) => {
+      deletePromises.push(deleteDoc(doc.ref));
+      // se crea un array vacio deletePromises para almacenar las promesas, despues se recorre
+      // la coleccion con forEach para tenerlos separados, despues se utiliza
+      // deletePromises.push para eliminar cada documento de la coleccion
+      // se eliminan todas las notas de la colección después de la prueba
+      // para evitar que queden notas de prueba en la base de datos
+    });
+    await Promise.all(deletePromises);
   });
 });
